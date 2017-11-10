118ee909caffa898b915912373f8bdf6c2cbba45
Rachel <rachel8973@gmail.com>
2017-11-10 20:25:35 +0000

    Reassembled as a test

--- Build.bat
+++ Build.bat
@@ -2,17 +2,22 @@
 CLS
 REM ===========================================================================
 REM   Build Z80 Forth
+REM
+REM   Assumes:
+REM       You are in the source directory
+REM       You have a path defined to the TASM.EXE on the C drive
+REM       You have set up the SET TASMTABS=C:/TASM or such like
+REM
 REM ===========================================================================
-SET TASMTABS=P:\Z80\TASM32\
-SET TASMEXE=%TASMTABS%TASM
-SET TASMOPTS=-80 -b -c
-SET SRCDIR=P:\Z80\Forth
+SET TASMEXE=C:\APPS\!TASM\DOS\TASM\TASM.EXE
+SET TASMTABS=C:\APPS\!TASM\DOS\TASM
+
 
 REM For ROM
-%TASMEXE% -80 -b -c -dMON %SRCDIR%\FORTH.Z80 %SRCDIR%\FORTHROM.bin %SRCDIR%\FORTHROM.lst
+%TASMEXE% -80 -b -c -dMON FORTH.ASM FORTHROM.BIN FORTHROM.LST
 
 REM For RAM
-REM %TASMEXE% -80 -g0 %SRCDIR%\FORTH.Z80 %SRCDIR%\FORTHRAM.hex %SRCDIR%\FORTHRAM.lst
+%TASMEXE% -80 -g0 FORTH.ASM FORTHRAM.HEX FORTHRAM.LST
 
 PAUSE
 
